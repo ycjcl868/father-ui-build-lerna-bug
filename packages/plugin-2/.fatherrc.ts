ec7cef1afd71020305e37c7f79fecef25ab85dfe
ycjcl868 <45808948@qq.com>
2019-09-03 18:21:20 +0800

    fix: lerna

--- packages/plugin-2/.fatherrc.ts
+++ packages/plugin-2/.fatherrc.ts
@@ -3,14 +3,4 @@ export default [
   {
     cjs: 'babel',
   },
-  {
-    entry: 'ui/index.tsx',
-    typescriptOpts: {
-      check: false,
-    },
-    umd: {
-      name: 'plugin-2',
-      minFile: false,
-    },
-  },
 ];
